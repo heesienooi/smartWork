a2ec494825efba807743bc57961624b963c1abf2
Heesien Ooi <heesien.ooi@gmail.com>
2013-10-06 17:10:24 +1100

    Add generate enketo url function to TaskModel

--- app/js/models/TaskModel.js
+++ app/js/models/TaskModel.js
@@ -4,7 +4,39 @@ define([
 ], function (_, Backbone) {
     'use strict';
 
-    var TaskModel = Backbone.Model.extend();
+    var TaskModel = Backbone.Model.extend({
+
+        generateEnketoUrl: function (callback) {
+            // TODO: abstract this hard code serverUrl to some settings model
+            var serverUrl = "http://rmit.smap.com.au";
+            var formId = this.get('task').form_id;
+
+            // Send ajax request to server to get enketo url
+            $.ajax({
+                type: "GET",
+                url: serverUrl+"/enketo/api_v1/survey",
+                data: {
+                    server_url: serverUrl,
+                    form_id: formId
+                },
+                dataType: 'jsonp',
+                success : function (data) {
+                    // TODO: abstract this hard code userId to some settings model
+                    var userId = 'admin'
+                    var url = data.smap_url + '&user=' + userId;
+
+                    callback(url);
+                },
+                error: function(xhr, textStatus, err) {
+                    if(xhr.readyState == 0 || xhr.status == 0) {
+                        return; // Not an error
+                    } else {
+                        console.log("Error: Failed to get web form: " + err);
+                    }
+                }
+            });
+        }
+    });
 
     return TaskModel;
 });
